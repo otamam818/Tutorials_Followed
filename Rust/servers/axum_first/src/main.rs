6e7b65d2cf2bef7417eda8897823ac9c67f5ff23
otamam818 <otamam818@gmail.com>
2022-12-16 13:03:02 +0600

    Completed axum basics tutorial

---
+++ Rust/servers/axum_first/src/main.rs
@@ -0,0 +1,67 @@
+use axum::{
+    routing::{get, post, get_service},
+    http::StatusCode,
+    response::IntoResponse,
+    Json, Router, extract::Path};
+use serde_json::json;
+use tokio::io;
+use tower_http::services::ServeFile;
+
+use std::net::SocketAddr;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize,Deserialize, Clone, Eq, Hash, PartialEq)]
+struct User {
+    id: u64,
+    username: String,
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[tokio::main]
+async fn main() {
+
+    tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/user", post(create_user))
+        .route("/static", get_service(ServeFile::new("static/hello.html"))
+        .handle_error(|error: io::Error| async move {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Unhandled internal error: {}", error),
+            )
+        }))
+        .route("/:name", get(json_hello));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, There!"
+}
+
+async fn json_hello(Path(name): Path<String>) -> impl IntoResponse {
+    let greeting = name.as_str();
+    let hello = String::from("Hello ");
+
+    (StatusCode::OK, Json(json!({"message": hello + greeting })))
+}
+
+async fn create_user(Json(payload): Json<CreateUser>,) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        username: payload.username
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
